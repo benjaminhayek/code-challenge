e7b423af766f7d6dc3b2aaa204ad6daea922b437
bhayek12@gmail.com <bhayek12@gmail.com>
2019-02-07 13:28:59 -0700

    Add dunction for checking capital

--- src/App/index.js
+++ src/App/index.js
@@ -7,22 +7,52 @@ class App extends Component {
     super(props)
     this.state = {
       randomStates: [],
+      randomCapital: '',
+      status: ''
     }
   }
 
   componentDidMount(){
-    this.getRandomStates()
+    let results = this.dataCleaner()
+    this.setState({randomStates: results})
+    let capital = results[Math.floor(Math.random()*4)]
+    this.setState({randomCapital: capital.capital})
+  }
+
+  dataCleaner = () => {
+    let states = this.getRandomStates()
+    let filtered = states.map(state => {
+      return {name: state.name, capital: state.capital, correct: false}
+    })
+    return filtered
   }
 
   getRandomStates = () => {
-    // let countries = Object.keys(mockData[Object.values(mockData)])
-    console.log(mockData)
+    let stateInfo = Object.values(mockData)
+    let fourRandom = []
+    for (let i = 0; i < 4; i++) { 
+    let randomState = stateInfo[Math.floor(Math.random()*stateInfo.length)]
+      fourRandom.push(randomState)
+    }
+    return fourRandom
+  }
+
+  divStyle = {border: '1px solid green'}
+
+  checkCapital = (value) => {
+    if(value.capital === this.state.randomCapital) {
+      this.setState({status: 'correct'})
+    } else {
+      this.setState({status: 'incorrect'})
+    }
   }
 
   render() {
     return (
       <div className="App">
         <h1>country game</h1>
+        <div>{this.state.randomStates.map(state => <div className={this.state.status === 'correct' ? 'right' : 'wrong'} value={state} onClick={() => this.checkCapital(state)} key={state.name}> {state.name} </div>)} </div>
+        <div value={this.state.randomCapital}>{this.state.randomCapital}</div>
       </div>
     );
   }
